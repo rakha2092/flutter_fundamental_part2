3e38140d0ce6993e9292db20bc5f4165557a2ae3
Rakhawidya <rakha2224@gmail.com>
2025-09-02 19:04:25 +0700

    layout_flutter

---
+++ README.md
@@ -0,0 +1,16 @@
+# layout_flutter
+
+A new Flutter project.
+
+## Getting Started
+
+![alt text](img/prak2.png)
+langkah pertama kita dapat membuat project baru, disini saya membuat dengan cara ctrl shift p untuk membuatnya, lalu buat layout pada flutter dengan cara seperti pada gambar
+![alt text](img/prak3.png)
+selanjutnya kita membuat column button yang berisi 3 kolom untuk tombol, lalu buat fungsi untuk menambahkan ikon lalu tambahkan buttonsection kebody
+![alt text](img/prak4.png)
+selanjutnya tambahkan teks untuk deskripsi dari wisata yang akan dibuat dengan menambahkan variabel text section kebody
+![alt text](img/prak5.png)
+implementasikan gambar yang telah disiapkan sebelumnya, siapkan folder img dan tambahkan gambar kesana lalu import ke file main.dart
+![alt text](img/prak.PNG)
+ini adalah hasil akhir untuk praktikum ini
---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          /* soal 1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /* soal 2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Wisata Air Terjun Tretes',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+               ) ,
+              ),
+              Text(
+                'Jombang, Indonesia',
+                style: TextStyle(color: Colors.grey,),
+              ),
+            ],
+          ),
+        ),
+        /* soal 3*/
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        const Text('41'),
+      ],
+    ),
+  );
+
+  Color color = Theme.of(context).primaryColor;
+
+  Column buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+  Widget buttonSection = Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      buildButtonColumn(color, Icons.call, 'CALL'),
+      buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+      buildButtonColumn(color, Icons.share, 'SHARE'),
+    ],
+  );
+
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: const Text(
+      'Sensasi udara sejuk pegunungan dan pemandangan air '
+      'terjun setinggi 158 meter menjadikan para pengunjungnya '
+      'betah berlama-lama di lokasi wisata ini. '
+      'Meski jaraknya cukup jauh, Air Terjun '
+      'Tretes yang disebut-sebut tertinggi di Jawa Timur ini tak pernah sepi pengunjung.'
+      '362458302092_Muhammad Rakha Widya Ardhana',
+     softWrap: true,
+    ),
+  );
+
+  return MaterialApp(
+    title: 'Flutter layout: Muhammad Rakha Widya Ardhana_362458302092',
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter layout demo'),
+      ),
+      body: ListView(
+        children: [
+          Image.asset(
+            'assets/1.jpg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          titleSection,
+          buttonSection,
+          textSection,
+        ],
+      ),
+    ),
+  );
+}
+}
